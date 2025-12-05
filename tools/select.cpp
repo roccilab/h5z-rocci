6f2d72869e359ad036775a9a38878ed3ec009e48
LangdaoZhang <zhanglangdao@outlook.com>
2025-12-05 14:13:13 -0500

    update select.cpp, this one is not important

--- tools/select.cpp
+++ tools/select.cpp
@@ -17,18 +17,97 @@ pressio library;
 
 using namespace std::string_literals;
 
-static const auto compressor_list = {"sz3", "zfp", "szx"};
+// static const auto compressor_list = {"sz3", "zfp", "szx"};
+static const auto compressor_list = {"szx"};
 
-double get_surrogate_cr(const std::string _, const pressio_data *input, const double eb);
-void compress_to_destination(const std::string _, const pressio_data *input, const double eb, const char *output_file);
+double get_surrogate_cr(const std::string _, pressio_data *input, const double eb);
+double get_real_cr(const std::string _, pressio_data *input, const double eb);
+void compress_to_destination(const std::string _, pressio_data *input, const double eb, const char *output_file);
 
-void compress_fix_eb_best_cr(const pressio_data *input, const double eb, const char *output_file);
-void compress_fix_psnr_best_cr(const pressio_data *input, const double psnr, const char *output_file);
-void compress_fix_ssim_best_cr(const pressio_data *input, const double ssim, const char *output_file);
+void compress_fix_eb_best_cr(pressio_data *input, const double eb, const char *output_file);
+void compress_fix_psnr_best_cr(pressio_data *input, const double psnr, const char *output_file);
+void compress_fix_ssim_best_cr(pressio_data *input, const double ssim, const char *output_file);
 
-void compress_fix_cr_best_eb(const pressio_data *input, const double cr, const char *output_file);
-void compress_fix_cr_best_psnr(const pressio_data *input, const double cr, const char *output_file);
-void compress_fix_cr_best_ssim(const pressio_data *input, const double cr, const char *output_file);
+void compress_fix_cr_best_eb(pressio_data *input, const double cr, const char *output_file);
+void compress_fix_cr_best_psnr(pressio_data *input, const double cr, const char *output_file);
+void compress_fix_cr_best_ssim(pressio_data *input, const double cr, const char *output_file);
+
+void print_list(auto &list) {
+    std::cout << "{";
+    for (int i = 0; i < list.size(); i++) {
+        std::cout << list[i];
+        if (i != list.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "}" << std::endl;
+}
+
+void print_list(auto &eb_list, auto &cr_list) {
+    print_list(eb_list);
+    print_list(cr_list);
+}
+
+void generate_compression_ratio_list(pressio_data *input, std::vector<double> &eb_list) {
+    std::vector<double> cr_list;
+    for (auto &eb : eb_list) {
+        cr_list.push_back(get_surrogate_cr("sz3", input, eb));
+    }
+    print_list(eb_list, cr_list);
+
+    cr_list.clear();
+    for (auto &eb : eb_list) {
+        cr_list.push_back(get_real_cr("sz3", input, eb));
+    }
+    print_list(eb_list, cr_list);
+
+}
+
+void generate_compression_ratio_list_linear(pressio_data *input) {
+    std::vector<double> eb_list;
+    for (int i = 0; i <= 15; i++) {
+        eb_list.push_back(1. * (i + 1) / 16);
+    }
+    generate_compression_ratio_list(input, eb_list);
+}
+
+void generate_compression_ratio_list_log(pressio_data *input) {
+    std::vector<double> eb_list;
+    for (int i = 0; i <= 15; i++) {
+        eb_list.push_back(pow(10, -1.0 * i / 3));
+    }
+    generate_compression_ratio_list(input, eb_list);
+}
+
+void generate_fix_compression_ratio_error_list(pressio_data *input) {
+    std::vector<double> cr_list;
+    // for (int i = 2; i <= 20; i++) cr_list.push_back(i);
+    for (int i = 12; i <= 20; i++) cr_list.push_back(i / 10.0);
+    std::vector<double> cr_surrogate_list, cr_real_list;
+
+    std::string _ = "zfp";
+
+    for (auto &cr : cr_list) {
+        double l = -1, r = 5, mid;
+
+        while(r - l > 1e-2) {
+            mid = (l + r) / 2.;
+            printf("%lf\n", pow(10, -mid));
+            auto current_cr = get_surrogate_cr(_, input, pow(10, -mid));
+            if (current_cr > cr) {
+                l = mid;
+            } else {
+                r = mid;
+            }
+        }
+
+        cr_surrogate_list.push_back(get_surrogate_cr(_, input, pow(10, -l)));
+        cr_real_list.push_back(get_real_cr(_, input, pow(10, -l)));
+    }
+
+    print_list(cr_list);
+    print_list(cr_surrogate_list, cr_real_list);
+}
 
 /*
 ./tools/select \
@@ -36,10 +115,27 @@ void compress_fix_cr_best_ssim(const pressio_data *input, const double cr, const
 3 500 500 100 \
 ~/scratch/tem eb_cr \
 1e-1
+
+./tools/select \
+/anvil/projects/x-cis240669/kai/data/hurricane-100x500x500/Uf48.bin.dat \
+3 500 500 100 \
+~/scratch/tem cr_eb \
+10
 */
 
+std::map<std::string, uint8_t> is_reversed_surrogate_dimension = {
+    {"sz3", 0}, 
+    {"zfp", 0}, 
+    {"szx", 0}, 
+    {"sperr", 0}, 
+    {"szp", 1}
+};
+std::map<std::string, uint8_t> &is_reversed_dimension = is_reversed_surrogate_dimension;
+
+std::vector<size_t> dims, reversed_dims;
+
 using function_t = std::function<void(
-    const pressio_data *, 
+    pressio_data *, 
     const double, 
     const char *)>;
 
@@ -55,7 +151,6 @@ std::unordered_map<std::string, function_t> func_mp = {
 signed main(int argc, char* argv[]) {
 
     char *input_file, *output_file;
-    std::vector<size_t> dims;
     double compression_parameter; //eb, psnr, ssim or cr, which are all in double type
     pressio_dtype type = pressio_float_dtype;
 
@@ -87,6 +182,9 @@ signed main(int argc, char* argv[]) {
     
     // Read the input data
 
+    reversed_dims = dims;
+    std::reverse(reversed_dims.begin(), reversed_dims.end());
+
     pressio_data metadata = pressio_data::owning(type, dims);
 
     pressio_io io = library.get_io("posix");
@@ -107,10 +205,12 @@ signed main(int argc, char* argv[]) {
     // testSZx_surrogate(input, eb);
     // testSZx(input, eb);
 
-    function(input, compression_parameter, output_file);
+    // function(input, compression_parameter, output_file);
+    // generate_compression_ratio_list_linear(input);
+    generate_fix_compression_ratio_error_list(input);
 }
 
-void compress_fix_eb_best_cr(const pressio_data *input, const double eb, const char *output_file) {
+void compress_fix_eb_best_cr(pressio_data *input, const double eb, const char *output_file) {
 
     double best_cr = 0;
     std::string best_compressor = "null";
@@ -127,23 +227,23 @@ void compress_fix_eb_best_cr(const pressio_data *input, const double eb, const c
     compress_to_destination(best_compressor, input, eb, output_file);
 }
 
-void compress_fix_psnr_best_cr(const pressio_data *input, const double psnr, const char *output_file) {
+void compress_fix_psnr_best_cr(pressio_data *input, const double psnr, const char *output_file) {
 
 
 }
 
-void compress_fix_ssim_best_cr(const pressio_data *input, const double ssim, const char *output_file) {
+void compress_fix_ssim_best_cr(pressio_data *input, const double ssim, const char *output_file) {
 
 }
 
-void compress_fix_cr_best_eb(const pressio_data *input, const double cr, const char *output_file) {
+void compress_fix_cr_best_eb(pressio_data *input, const double cr, const char *output_file) {
 
     double best_eb = -1;
     std::string best_compressor = "null";
 
     for (const auto &_ : compressor_list) {
 
-        double l = 0, r = 5, mid;
+        double l = -1, r = 5, mid;
         // 10 ^ -
 
         while(r - l > 1e-2) {
@@ -165,18 +265,27 @@ void compress_fix_cr_best_eb(const pressio_data *input, const double cr, const c
     }
 
     std::cout << "Best compressor: " << best_compressor << " with EB: " << best_eb << std::endl;
+
+    compress_to_destination(best_compressor, input, best_eb, output_file);
 }
 
-void compress_fix_cr_best_psnr(const pressio_data *input, const double cr, const char *output_file) {
+void compress_fix_cr_best_psnr(pressio_data *input, const double cr, const char *output_file) {
 
 }
 
-void compress_fix_cr_best_ssim(const pressio_data *input, const double cr, const char *output_file) {
+void compress_fix_cr_best_ssim(pressio_data *input, const double cr, const char *output_file) {
 
 }
 
 
-double get_surrogate_cr(const std::string _, const pressio_data *input, const double eb) {
+double get_surrogate_cr(std::string _, pressio_data *input, const double eb) {
+
+    if (is_reversed_dimension[_]) {
+        pressio_data_reshape(input, reversed_dims.size(), reversed_dims.data());
+    }
+    else {
+        pressio_data_reshape(input, dims.size(), dims.data());
+    }
 
     const std::string &compressor_string = _;
     auto compressor = library.get_compressor(_ + "_surrogate");
@@ -214,7 +323,7 @@ double get_surrogate_cr(const std::string _, const pressio_data *input, const do
     auto metrics = compressor->get_metrics_results();
     auto assert_defined = [&](const char* key, auto value){
         if(metrics.get(key, value)!= pressio_options_key_set) {
-            throw std::runtime_error(key + " was note set"s);
+            throw std::runtime_error(key + " was not set"s);
         }
     };
 
@@ -224,7 +333,7 @@ double get_surrogate_cr(const std::string _, const pressio_data *input, const do
     return cr;
 }
 
-double get_surrogate_psnr(const std::string _, const pressio_data *input, const double eb) {
+double get_surrogate_psnr(const std::string _, pressio_data *input, const double eb) {
 
     const std::string &compressor_string = _;
     auto compressor = library.get_compressor(_ + "_surrogate");
@@ -253,7 +362,7 @@ double get_surrogate_psnr(const std::string _, const pressio_data *input, const 
     auto metrics = compressor->get_metrics_results();
     auto assert_defined = [&](const char* key, auto value){
         if(metrics.get(key, value)!= pressio_options_key_set) {
-            throw std::runtime_error(key + " was note set"s);
+            throw std::runtime_error(key + " was not set"s);
         }
     };
 
@@ -263,7 +372,53 @@ double get_surrogate_psnr(const std::string _, const pressio_data *input, const 
     return psnr;
 }
 
-void compress_to_destination(const std::string _, const pressio_data *input, const double eb, const char *output_file) {
+double get_real_cr(const std::string _, pressio_data *input, const double eb) {
+
+    pressio_data_reshape(input, dims.size(), dims.data());
+
+    std::string compressor_string = _;
+    auto compressor = library.get_compressor(compressor_string);
+    if (!compressor) {
+        std::cerr << "Failed to load " << compressor_string << " compressor" << std::endl;
+        exit(-1);
+    }
+
+    pressio_options compression_options{
+        {"sz3:metric", "composite"},
+        {"composite:plugins", std::vector{"size"s}},
+        {"pressio:abs", eb}
+    };
+
+    if(compressor->set_options(compression_options) != 0) {
+        std::cerr << "Failed to set options: " << compressor->error_msg() << std::endl;
+        exit(-1);
+    }
+
+    pressio_data compressed = pressio_data::empty(pressio_byte_dtype, {});
+
+    if(compressor->compress(input, &compressed) != 0) {
+        std::cerr << "Compression error: " << compressor->error_msg() << std::endl;
+        exit(-1);
+    }
+
+    double cr = 1. * input->size_in_bytes() / compressed.size_in_bytes();
+    return cr;
+
+
+    // auto metrics = compressor->get_metrics_results();
+    // auto assert_defined = [&](const char* key, auto value){
+    //     if(metrics.get(key, value)!= pressio_options_key_set) {
+    //         throw std::runtime_error(key + " was not set"s);
+    //     }
+    // };
+
+    // double cr;
+    // // assert_defined("error_stat:psnr", &cr);
+    // assert_defined("size:compression_ratio", &cr);
+    // return cr;
+}
+
+void compress_to_destination(const std::string _, pressio_data *input, const double eb, const char *output_file) {
 
     std::string compressor_string = "sz3";
     auto compressor = library.get_compressor(compressor_string);
@@ -293,7 +448,7 @@ void compress_to_destination(const std::string _, const pressio_data *input, con
     auto metrics = compressor->get_metrics_results();
     auto assert_defined = [&](const char* key, auto value){
         if(metrics.get(key, value)!= pressio_options_key_set) {
-            throw std::runtime_error(key + " was note set"s);
+            throw std::runtime_error(key + " was not set"s);
         }
     };
 
@@ -360,7 +515,7 @@ void compress_to_destination(const std::string _, const pressio_data *input, con
 //     auto metrics = compressor->get_metrics_results();
 //     auto assert_defined = [&](const char* key, auto value){
 //         if(metrics.get(key, value)!= pressio_options_key_set) {
-//             throw std::runtime_error(key + " was note set"s);
+//             throw std::runtime_error(key + " was not set"s);
 //         }
 //     };
 
@@ -403,7 +558,7 @@ void compress_to_destination(const std::string _, const pressio_data *input, con
 //     auto metrics = compressor->get_metrics_results();
 //     auto assert_defined = [&](const char* key, auto value){
 //         if(metrics.get(key, value)!= pressio_options_key_set) {
-//             throw std::runtime_error(key + " was note set"s);
+//             throw std::runtime_error(key + " was not set"s);
 //         }
 //     };
 
@@ -445,7 +600,7 @@ void compress_to_destination(const std::string _, const pressio_data *input, con
 //     auto metrics = compressor->get_metrics_results();
 //     auto assert_defined = [&](const char* key, auto value){
 //         if(metrics.get(key, value)!= pressio_options_key_set) {
-//             throw std::runtime_error(key + " was note set"s);
+//             throw std::runtime_error(key + " was not set"s);
 //         }
 //     };
 
@@ -488,7 +643,7 @@ void compress_to_destination(const std::string _, const pressio_data *input, con
 //     auto metrics = compressor->get_metrics_results();
 //     auto assert_defined = [&](const char* key, auto value){
 //         if(metrics.get(key, value)!= pressio_options_key_set) {
-//             throw std::runtime_error(key + " was note set"s);
+//             throw std::runtime_error(key + " was not set"s);
 //         }
 //     };
 
@@ -530,7 +685,7 @@ void compress_to_destination(const std::string _, const pressio_data *input, con
 //     auto metrics = compressor->get_metrics_results();
 //     auto assert_defined = [&](const char* key, auto value){
 //         if(metrics.get(key, value)!= pressio_options_key_set) {
-//             throw std::runtime_error(key + " was note set"s);
+//             throw std::runtime_error(key + " was not set"s);
 //         }
 //     };
 
@@ -573,7 +728,7 @@ void compress_to_destination(const std::string _, const pressio_data *input, con
 //     auto metrics = compressor->get_metrics_results();
 //     auto assert_defined = [&](const char* key, auto value){
 //         if(metrics.get(key, value)!= pressio_options_key_set) {
-//             throw std::runtime_error(key + " was note set"s);
+//             throw std::runtime_error(key + " was not set"s);
 //         }
 //     };
 
